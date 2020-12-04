1af18f515edfd040a9605474a58313f689e1b8a4
ath331 <kstnftk@naver.com>
2020-12-04 23:21:28 +0900

    20.12.04 소수 찾기

--- programmers/programmers/main.cpp
+++ programmers/programmers/main.cpp
@@ -1,40 +1,58 @@
+//소수 찾기 https://programmers.co.kr/learn/courses/30/lessons/42839
+
 #include <string>
-#include <vector>
-#include <bitset>
+#include <cmath>
 
 using namespace std;
 
+bool Visit[10000000];
+bool Select[10];
+int Answer;
+
+bool IsSosu(int num)
+{
+	if (num == 0 || num == 1)
+		return false;
+
+	for (int i = 2; i < sqrt(num); i++) //최대값을 루트로 판별? 참조 https://yabmoons.tistory.com/336
+	{
+		if (num % i == 0)
+			return false;
+	}
+
+	return true;
+}
+
+void DFS(int Cnt, string S, string Res)
+{
+    if (Res != "" && Visit[stoi(Res)] == false)
+    {
+        int Num = stoi(Res);
+        Visit[Num] = true;
+        if (IsSosu(Num) == true)
+            Answer++;
+    }
+
+    for (int i = 0; i < S.length(); i++)
+    {
+        if (Select[i] == true) 
+            continue;
+
+        Select[i] = true;
+        DFS(Cnt + 1, S, Res + S[i]);
+        Select[i] = false;
+    }
+}
+
 int solution(string numbers) {
-	int answer = 0;
+    DFS(0, numbers, "");
 
-	int maxCount = 1;
-	for (int i = 0; i < numbers.size(); i++)
-	{
-		maxCount *= 2;
-	}
-
-	vector<int> v;
-	for (int i = 1; i < maxCount; i++)
-	{
-		string numBit = bitset<7>(i).to_string(); //bit size 고정
-		numBit = numBit.substr(numBit.size() - numbers.size()); //bit size 보정
-
-		string temp = "";
-		for (int bi = 0; bi < numBit.size(); bi++)
-		{
-			if (numBit[bi] == '1')
-				temp += numbers[bi];
-		}
-
-		v.push_back(stoi(temp));
-	}
-
-	return answer;
+	return Answer;
 }
 
 int main()
 {
-	solution("817");
+	solution("17");
 
 	return 0;
 }
